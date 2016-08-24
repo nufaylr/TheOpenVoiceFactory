2235613f1642dbe13a8a5e164b8b4313380aa4ec
Joe Reddington <joe@joereddington.com>
2016-08-24 13:40:23 +0100

    Blanking this branch so that it can be used as a basis for gh-changes

--- create.sh
+++
@@ -1,8 +0,0 @@
-filename=$(basename "$1")
-extension="${filename##*.}"
-dest="${filename%.*}"
-echo $dest
-mkdir $dest
-python grab_text.py $1 $dest $2
-cp -R templates/* $dest
-perl -pi -e "s/XXX/$3/" $dest/*.pl
